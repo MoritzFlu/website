b391545e4557e24ca7b5edec1ec51db8d19afa44
Moritz Flüchter <moritz.fluechter@uni-tuebingen.de>
2023-12-30 00:29:45 +0100

    Formatting

--- src/packets.js
+++ src/packets.js
@@ -1,111 +1,86 @@
 
-
-let speed = 2;
-let spawn_rate = 0.015;
-
-let cables;
-let svg;
-
+// Vars to tweak packet spawn rate, speed and possible colors
+let speed = 1.5;
+let spawn_interval = 0.2;
 let colors = [
     "#00FF00",
     "#00FF00",
     "#00FF00"
 ];
 
+// reference holders
+let cables;
+let svg;
 let id;
 
-function move_packet(packet) {
 
-}
-
-
-function start() {
-    cables = svg.getElementsByClassName("cable");
-
+function spawn() {
+    // Select a random cable
     cable = cables[Math.floor(Math.random()*cables.length)]
-
+    // Spawn packet for that cable
     spawn_packet(cable);
 }
 
 
 function spawn_packet(cable) {
 
-    // To select
-    // speed in seconds
-    // size
-    // color
-
+    // Get color for packet to spawn
     let color = colors[Math.floor(Math.random() * colors.length)];
 
-    //console.log(cable);
+    // Create packet SVG object
     let new_packet = document.createElementNS("http://www.w3.org/2000/svg", 'circle');
-
-    new_packet.setAttribute("r","7.4");
+    new_packet.setAttribute("id",id);
+    new_packet.setAttribute("r","3.5");
     new_packet.setAttribute("fill",color);
 
+    // Get animation path and direction
     let packet_path = cable.getAttribute("d");
+    let direction = Math.random() > 0.5 ? "normal" : "reverse";
 
-    //new_packet.classList.add("packet");
-    new_packet.style.animation = "ball-move "+speed+"s linear 1";
-    new_packet.style.offset = "path('"+packet_path+"') 0% auto";
-    //new_packet.style.offsetPath = "path("+packet_path+")";
+    //Set animation for packet
+    let style = new_packet.style;
+    style.animationDirection = direction;
+    style.animationName = "ball-move";
+    style.animationDuration = speed + "s";
+    style.animationTimingFunction = "linear";
+    style.animationIterationCount = "1";
 
+    // Set animation path for packet
+    style.offsetPath = "path('"+packet_path+"')";
+    style.offsetDistance = "0%";
+    
+    // Add generated packet to webpage SVG
+    svg.appendChild(new_packet);
 
-    new_packet.id = id;
-    //new_packet.style.animationDuration = speed;
-    //new_packet.style.offsetPath = path;
-
+    // Trick: id will be changed by future calls but is needed to delete element
+    // This stores the id locally for the setTimeout function call and then increments the global id
     let loc_id = id;
     id = id + 1;
-
-
-    //let container = document.createElement("object");
-    //container.setAttribute("type","image/svg+xml");
-    //container.id = id;
-
-
-    //let anim = document.createElementNS("http://www.w3.org/2000/svg","animateMotion");
-
-    //anim.setAttribute("dur",speed);
-
-    // Has to be indefinite, otherwise the animation will not repeat for
-    // another packet on the same path!
-    // Even if the SVG object is remove inbetween animations
-    //anim.setAttribute("repeatCount","indefinite");
-    //anim.setAttribute("fill","freeze");
-
-    //anim.setAttribute("path",path);
-
-    //new_packet.appendChild(anim);
-    //target.appendChild(new_packet);
-
-    //container.appendChild(new_packet);
-    //svg.appendChild(container);
-
-    svg.appendChild(new_packet);
-    //new_packet.beginElement();
-
+    // Delete Packets once they arrive at destination
     setTimeout(function(){
         svg.getElementById(loc_id).remove();
     },speed*1000);
-
-    // <circle r="7.4" fill="#979797">
-    //<animateMotion dur="10s" repeatCount="1" fill="freeze" path="M7 53 2 23C1 11 10-1 22 0c14 1 29 20 35 31l22 57 7 10c3 3 33 27 39 10 10-24 5-57 6-81 1-7-2-18 4-20l3-4c42-11 52 40 63 67 10 23 31 26 45 5l1-6" />
-    //</circle>
-
 }
 
+// Load SVG and cables on document load and start packet spawn
 document.addEventListener("DOMContentLoaded", function() {
+
+    // Reference to SVG object
     var svg_src = document.getElementById("svg-object");
+
+    // Initialize packet SVG object IDs
     id = 0;
     svg_src.addEventListener("load",function() {
+
+        // Set reference to SVG content
         svg = svg_src.contentDocument.getElementById("svg");
-        start();
+
+        // Set reference to all cables in SVG
+        cables = svg.getElementsByClassName("cable");
+
+        // Start packet spawn
+        var intervalId = window.setInterval(function(){
+            spawn();
+        }, spawn_interval*1000);
     })
 });
-
-
-var intervalId = window.setInterval(function(){
-    // call your function here
-    start();
-  }, speed*1000*0.5);
